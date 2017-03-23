42c8f706531afe1296fe7d2df94c99bd0072ba3d
gick <gicquel.pierreyves@gmail.com>
2017-03-23 19:54:25 +0100

    update dependencies

--- app/config/database.js
+++ app/config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = [{
 
-    'url': 'mongodb://localhost/test' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url': 'mongodb://109.235.69.105:27017/test' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 }, { 'url': 'mongodb://localhost/game' }];
 	 
