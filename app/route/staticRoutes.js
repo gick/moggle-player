ebf9c0917f2e9dcd703768d93ac7d4707b6df363
gick <gicquel.pierreyves@gmail.com>
2018-04-16 15:34:44 +0200

    de

--- app/route/staticRoutes.js
+++ app/route/staticRoutes.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
 
-    var webdir = require('../config/localconfig.js')
+    var webdir = require('../config/config.js')
     var express = require('express');
     // only one route to serve the static site
     app.use(express.static(webdir));
